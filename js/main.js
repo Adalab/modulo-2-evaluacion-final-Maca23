8053610439c4c81ab9bc0765f85d705379e3a743
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-24 14:18:00 +0200

    add css

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@ const searchInput = document.querySelector(".js-search-input");
 const ulSeries = document.querySelector(".js-series");
 const seriesFav = document.querySelector(".js-series-fav");
 
-//SEARCH
-//Al hacer clic sobre el botón de Buscar, la aplicación debe conectarse al API abierto de TVMaze para la búsqueda de series
 function searchSeries() {
   arraySeries = [];
   const searchInputValue = searchInput.value;
@@ -24,7 +22,6 @@ function searchSeries() {
     });
 }
 
-//Por cada serie contenido en el resultado de la búsqueda hay que pintar una tarjeta donde mostramos una imagen de la serie y el título. Algunas de las series que devuelve el API no tienen imagen. En ese caso hay que mostrar una imagen de relleno
 function getHtmlCode(series) {
   let htmlCode = "";
   for (const serie of series) {
@@ -41,6 +38,44 @@ function getHtmlCode(series) {
   return htmlCode;
 }
 
+function getHtmlCodeFav(favorites) {
+  let htmlCode = "";
+  for (const serie of favorites) {
+    htmlCode += `<li class="cards-fav js-cards" id= "${serie.id}">`;
+    if (serie.image === null) {
+      htmlCode += `<img class="cards-content-img" src="${"https://via.placeholder.com/210x295/ffffff/666666/?text=TV"}" />`;
+    } else {
+      htmlCode += `<img class="cards-img-fav" src="${serie.image.medium}" />`;
+    }
+    htmlCode += `<h2 class="cards-title-fav">${serie.name}</h2>`;
+    htmlCode += `<span class="close js-close" id="${serie.id}">x</span>`;
+    htmlCode += `</li>`;
+  }
+
+  return htmlCode;
+}
+
+function addEventListenerClose() {
+  const close = document.querySelectorAll(".js-close");
+  for (const listClose of close) {
+    listClose.addEventListener("click", handleListClose);
+  }
+}
+
+function handleListClose(event) {
+  let id = parseInt(event.currentTarget.id);
+  let positionId = favorites.findIndex((fav) => id === fav.id);
+  favorites.splice(positionId, 1);
+  paintSeriesFav(favorites);
+  addEventListenerClose();
+}
+
+function reset() {
+  favorites = [];
+  seriesFav.innerHTML = "";
+  const resetBtn = document.querySelector('.js-reset-btn');
+}
+
 function paintSeries(arraySeries) {
   let htmlCode = getHtmlCode(arraySeries);
   ulSeries.innerHTML = htmlCode;
@@ -53,15 +88,15 @@ function addEventListenerToCards() {
     cards.addEventListener("click", handleFavList);
   }
 }
-searchBtn.addEventListener("click", searchSeries);
-addEventListenerToCards() 
 
-//Una vez aparecen los resultados de búsqueda, la usuaria puede indicar cuáles son nuestras series favoritas
+searchBtn.addEventListener("click", searchSeries);
+addEventListenerToCards();
+
 function handleFavList(ev) {
   const upDateFav = ev.currentTarget; //para coger toda la info, target es solo para coger info en concreto
   let favId = ev.currentTarget.id;
   const showFav = favorites.find((element) => element.id === parseInt(favId));
-  if (showFav == null) {
+  if (showFav === undefined) {
     const selectedSeriesFav = arraySeries.find(
       (element) => element.id === parseInt(favId)
     );
@@ -73,20 +108,23 @@ function handleFavList(ev) {
     favorites.splice(selectedSeriesFav2, 1);
     upDateFav.classList.add("cards");
     upDateFav.classList.remove("seriescolor");
-  
   }
-  localStorage.setItem('series', JSON.stringify(favorites));
+  localStorage.setItem("series", JSON.stringify(favorites));
   paintSeriesFav(favorites);
+  addEventListenerClose();
 }
 
 function paintSeriesFav(favorites) {
-  let htmlCode = getHtmlCode(favorites);
+  let htmlCode = getHtmlCodeFav(favorites);
   seriesFav.innerHTML = htmlCode;
 }
 
-const saveSeriesFav = JSON.parse(localStorage.getItem('series'));
-if (saveSeriesFav) {
+function getLocalStorage() {
+  const saveSeriesFav = JSON.parse(localStorage.getItem("series"));
+  if (saveSeriesFav) {
     favorites = saveSeriesFav;
-};
-
-paintSeriesFav(favorites);
+    paintSeriesFav(favorites);
+    addEventListenerClose();
+  }
+}
+getLocalStorage();
